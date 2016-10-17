7eec3ac69ac80478127f6901f206b150ae3ddd0c
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2016-10-17 11:01:34 +0200

    Removed preceding underscores in pthRootOfUnity class member variable names.

--- pynitefields/pthrootofunity.py
+++ pynitefields/pthrootofunity.py
@@ -15,16 +15,16 @@ class pthRootOfUnity():
     def __init__(self, p, e = 1):
         """ Initialize a pth root of unity.
             Member variables:
-            _p - The characteristic of the underlying field. Should be prime.
+            p - The characteristic of the underlying field. Should be prime.
                  Element takes the form of exp(2 pi i / p)
-            _e - The exponent, e.g. w^e. Default is 1.
+            e - The exponent, e.g. w^e. Default is 1.
         """
         # TODO some sort of primality check
         if p < 2:
             print ("Error, please enter a prime to make roots of unity.")
         else:
-            self._p = p
-            self._e = e
+            self.p = p
+            self.e = e
 
 
     def __mul__(self, op):
@@ -33,12 +33,12 @@ class pthRootOfUnity():
             and so on. So we just need to add the exponents and get the
             result mod p.
         """
-        if self._p != op._p:
+        if self.p != op.p:
             print("Error, cannot multiply roots of unity from different primes.")
             return 
 
-        new_exp = (self._e + op._e) % self._p
-        return pthRootOfUnity(self._p, new_exp)  
+        new_exp = (self.e + op.e) % self.p
+        return pthRootOfUnity(self.p, new_exp)  
 
 
     def __imul__(self, op):
@@ -48,12 +48,12 @@ class pthRootOfUnity():
 
     def __truediv__(self, op):
         """ Division. Same idea as multiplication. """
-        if self._p != op._p:
+        if self.p != op.p:
             print("Error, cannot divide roots of unity from different primes.")
             return 
 
-        new_exp = (self._e - op._e) % self._p
-        return pthRootOfUnity(self._p, new_exp)  
+        new_exp = (self.e - op.e) % self.p
+        return pthRootOfUnity(self.p, new_exp)  
 
 
     def __itruediv__(self, op):
@@ -64,28 +64,28 @@ class pthRootOfUnity():
     def __pow__(self, exponent):
         """ Exponentiation. Just multiply by exponent and take it
             modulo the prime. """
-        new_exp = (self._e * exponent) % self._p
-        return pthRootOfUnity(self._p, new_exp)  
+        new_exp = (self.e * exponent) % self.p
+        return pthRootOfUnity(self.p, new_exp)  
 
             
     def __eq__(self, op):
         """ Test equality of two field elements """
-        if self._p != op._p:
+        if self.p != op.p:
             return False
-        if self._e != op._e:
+        if self.e != op.e:
             return False
         return True
 
 
     def __repr__(self):
         """ Make the field element get printed in the command line."""
-        return ("w^" + str(self._e))
+        return ("w^" + str(self.e))
 
 
     def eval(self):
         """ Compute the actual number. Gross. """
-        return pow(np.exp(2 * np.pi * 1j / self._p), self._e)
+        return pow(np.exp(2 * np.pi * 1j / self.p), self.e)
 
 
     def print(self):
-        print("w^" + str(self._e))
+        print("w^" + str(self.e))
