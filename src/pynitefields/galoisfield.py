781da2a019c1d2490fc639bffabd2853351f79df
Olivia <dimatteo.olivia@gmail.com>
2016-11-22 10:18:06 -0500

    Made all negative indexing possible for GaloisField.

--- src/pynitefields/galoisfield.py
+++ src/pynitefields/galoisfield.py
@@ -156,7 +156,7 @@ class GaloisField():
 
               None if idx is out of bounds.
         """
-        if idx < self.dim and idx >= -1:
+        if idx < self.dim and idx >= (-1 * self.dim):
             return self.elements[idx]
         else:
             print("Error, element out of bounds.")
